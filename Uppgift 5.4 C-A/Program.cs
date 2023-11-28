97032c633d163c6fd1b133723b47dc7ba463ad82
Dijar06 <dize06001@utb.vaxjo.se>
2023-11-28 15:01:29 +0100

    Add project files.

---
+++ Uppgift 5.4 C-A/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[5] { "Ett", "Två", "Tre", "Fyra", "Fem" };
+            string storst = namn[0];
+            for (int i = 0; i < namn.Length; i++)
+            {
+                if (namn[i].CompareTo(storst) < 0)
+                {
+                    storst = namn[i];
+                }
+            }
+            Console.WriteLine("Första är " + storst);
+        }
+    }
+}
